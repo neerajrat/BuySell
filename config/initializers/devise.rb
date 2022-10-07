3a550567b839df33a811e1c208cd93bb82081ae3
neerajrat <nerathor@bestpeers.com>
2022-10-07 11:18:10 +0530

    mailconfirmation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -266,7 +266,8 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  #config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
@@ -309,4 +310,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   config.navigational_formats = ['*/*', :html, :turbo_stream]
+  config.scoped_views = true
 end
