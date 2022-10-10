9704d518d4f313f9380d20526f42edd3d6975bd3
neerajrat <nerathor@bestpeers.com>
2022-10-10 16:10:23 +0530

    Add product table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  default_url_options :host => "example.com"
   devise_for :users, controllers: {
     sessions: 'users/sessions'
     }
@@ -7,6 +8,17 @@ Rails.application.routes.draw do
   root "home#index"
   get  "/home/user", to:  "home#user"
   get  "/home/admin", to:  "home#admin"
+
+  get  "/products/index", to:  "products#index"
+  get  "/products/:id", to: "products#show"
+
+
+  resources :users do
+    resources:products
+  end
+  resource:contact
+
+
 
 
   end
