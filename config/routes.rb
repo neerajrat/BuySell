c19c692681a08d4dfa0cc37695593e559cd11d77
neerajrathore <nerathor@bestpeers.com>
2022-11-08 18:50:09 +0530

    Added Valdiation For models & fix columns name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'categories/index'
 
   root "home#index"
-
+  resources:categories
   default_url_options :host => "example.com"
   devise_for :users, controllers: {
     sessions: 'users/sessions'
@@ -16,14 +17,23 @@ Rails.application.routes.draw do
 
   get  "/products/index", to:  "products#index"
   get  "/products/sell", to:  "products#sell"
-  get  "/products/search", to:  "products#search"
+  get  "/search", to:  "products#search"
+  get  "/search_category", to:  "products#search_category"
+
   get  "/products/myproduct", to:  "products#myproduct"
   get  "/products/:id", to: "products#show"
   get  "/product/accept", to:  "products#accept"
   get  "/product/reject", to:  "products#reject"
+  get  "/product/offeraccept", to:  "products#offeraccept"
+
   get  "/product/productname", to:  "products#productname"
   get  "/product/pending", to:  "products#pending"
   get  "/product/offer", to:  "products#offer"
+  get  "/product/proposal", to:  "products#proposal"
+  get  "/view", to:  "proposals#view"
+
+
+
 
 
   resources:products, only:[:new , :create , :destroy, :edit , :update] 
@@ -32,6 +42,13 @@ Rails.application.routes.draw do
     resources:products
   end
 
-  resource:contact
+  resources :prodcut do
+    member do
+      resource:proposals, only:[:new, :create, :edit, :update] 
+      get  "/proposals", to:  "proposals#index"
 
+    end
+  end
+
+  resource:contact
   end
